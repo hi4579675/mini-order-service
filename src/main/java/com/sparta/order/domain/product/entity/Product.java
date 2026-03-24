e06a9c2171e5b7f87c9b01e37315c276f952a466
hi4579675 <hi4579675@gmail.com>
2026-03-24 21:27:29 +0900

    feat: 상품 삭제 soft delete 도입

--- src/main/java/com/sparta/order/domain/product/entity/Product.java
+++ src/main/java/com/sparta/order/domain/product/entity/Product.java
@@ -7,10 +7,15 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+// JPA는 내부적으로 프록시 객체 생성 시 기본 생성자가 필요한데 public으로 열면 new Product() 처럼 아무데서나 생성이 가능해짐
+// -> protected로 막아서 직접 생성 차단, JPA만 사용 가능하게 제한
+@SQLRestriction("is_deleted = false")
+// // WHERE is_deleted = false 조건이 붙음
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,13 +31,17 @@ public class Product {
     private Integer stock;
 
     @Builder
+    // 빌더 직접 쓰는걸 막기 위해 private로 했음
+    // 아래 정적 팩토리 메서드(createProduct)를 통해서만 생성하도록 강제
     private Product(String name, Long price, Integer stock){
         this.name = name;
         this.price = price;
         this.stock = stock;
     }
 
-    // 정적 팩토리 메서드 :
+    // 정적 팩토리 메서드 : new Product() 대신 Product.createProduct()로 생성하게
+    // 1. 메서드 이름으로 의도를 명확하게 표현 가능하단 장점이 있음
+    // 2. 생성 전 검증 로직, 비즈니스 로직 추가 가능함
     public static Product createProduct(String name, Long price, Integer stock){
         return Product.builder()
                 .name(name)
@@ -42,18 +51,32 @@ public class Product {
     }
 
     // 상품 수정을 위한 메서드
+    // 엔티티에는 setter 사용 x
+    // update()로 캡슐화 하여 수정이라는 행위가 엔티티 안에서만 이뤄지게 하고 싶었음
     public void update(String name, Long price, Integer stock){
         this.name = name;
         this.price = price;
         this.stock = stock;
     }
 
-    // 재고 차감
+    // 재고 차감 메서드
+    // Service에서도 할 수 있었지만 재고 차감은 Product의 책임이라고 생각하여 여기에 위치시킴
     public void removeStock(int quantity){
         int restStock = this.stock - quantity;
         if (restStock < 0){
+            // 재고 부족 시 커스텀 예외 발생, 서비스 레이어가 아닌 도메인에서 직접 검증
             throw new ApiException(ErrorStatus.LOW_STOCK);
         }
         this.stock = restStock;
+    }
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+    // 삭제 여부 플래그
+    // false = 정상, true = 삭제됨
+
+    // soft delete 메서드
+    public void delete(){
+        this.isDeleted = true;
     }
 }
--- src/main/java/com/sparta/order/domain/product/service/ProductService.java
+++ src/main/java/com/sparta/order/domain/product/service/ProductService.java
@@ -22,6 +22,7 @@ public class ProductService {
     @Transactional
     public Long create(ProductRequestDto dto) {
         Product product = Product.createProduct(dto.getName(), dto.getPrice(), dto.getStock());
+        // 정적 팩토리 메서드로 생성해서 new Project()직접 호출 불가
         return productRepository.save(product).getId();
     }
 
@@ -36,16 +37,25 @@ public class ProductService {
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new ApiException(ErrorStatus.PRODUCT_NOT_FOUND));
         product.update(dto.getName(), dto.getPrice(), dto.getStock());
+        //  productRepository.save(product)를 호출하지 않아도 DB에 반영됨!
+        // 이유: @Transactional 안에서 조회한 엔티티는 영속성 컨텍스트가 관리함
+        // 트랜잭션 종료 시 변경된 필드를 감지(dirty checking)해서 UPDATE 쿼리 자동 실행
     }
 
     public List<ProductResponseDto> getAllProducts() {
         List<Product> products = productRepository.findAll();
 
-        return products.stream().map(ProductResponseDto::from).toList();
+        return products.stream() // List를 스트림으로 변환 → 함수형 방식으로 처리
+                              .map(ProductResponseDto::from)// 각 Product 엔티티를 ProductResponseDto로 변환
+                               .toList(); // 스트림을 다시 List로 수집
     }
 
     @Transactional
     public void delete(Long id) {
-        productRepository.deleteById(id);
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ApiException(ErrorStatus.PRODUCT_NOT_FOUND));
+        // 존재하지 않는 ID 방어 처리
+        product.delete(); // isDeleted = true로 변경
+
     }
 }
