caa06d7819822b835dbfd4e4945c11b210670ef9
hi4579675 <hi4579675@gmail.com>
2026-03-24 17:14:27 +0900

    feat: 공통 성공 상태 코드(SuccessStatus) 정의 및 구현

---
+++ src/main/java/com/sparta/order/cofing/error/status/SuccessStatus.java
@@ -0,0 +1,26 @@
+package com.sparta.order.cofing.error.status;
+
+import com.sparta.order.cofing.error.dto.ResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+        OK(HttpStatus.OK, "COMMON200", "성공입니다. "),
+        CREATE(HttpStatus.CREATED, "COMMON201", "저장에 성공하였습니다.");
+
+        private final HttpStatus httpStatus;
+        private final String code;
+        private final String message;
+
+    @Override
+    public ResponseDto getReasonHttpStatus() {
+        return ResponseDto.builder()
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
